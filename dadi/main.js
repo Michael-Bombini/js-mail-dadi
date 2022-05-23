89f2c63466afd9e6eda2e842ed198e6ed68d9a02
Michael-Bombini <bombini.michael@gmail.com>
2022-05-23 14:37:18 +0200

    Added exercise  2 still not completed

---
+++ dadi/main.js
@@ -0,0 +1,56 @@
+/*
+Gioco dei dadi
+Generare più  numeri random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+*/
+
+//recupero elementi dal DOM
+const genera = document.querySelector(".genera");
+const controlla = document.querySelector(".controlla");
+const container = document.querySelector(".container");
+
+//dichiarazione array e rispettivo totale su cui sarà calcolata la media
+const estrattiComputer = [];
+const estrattiGiocatore = [];
+let totaleComputer = 0 ;
+let totaleGiocatore = 0 ;
+
+
+//ascolto l'evento click su genera e genero due numeri rispettivi
+genera.addEventListener("click" , function(){
+
+let generatoComputer = 0 ;
+let generatoGiocatore = 0 ;
+
+//genero due numeri casuali tra 1 e 6 uno per il computer e uno per l'utente
+generatoComputer = (Math.floor(Math.random() * 6 ) + 1 );
+generatoGiocatore = (Math.floor(Math.random() * 6 ) + 1 );
+
+//aggiungo quei numeri generati nel vettore
+estrattiComputer.push(generatoComputer);
+estrattiGiocatore.push(generatoGiocatore);
+
+container.innerHTML += `<div>Numero generato computer : ${generatoComputer}</div>`;
+container.innerHTML += `<div>Numero generato giocatore : ${generatoGiocatore}</div>`;
+
+
+});
+
+
+//ascolto l'evento click su controlla e con la funzione faccio i dovuti calcoli
+//somma e media
+controlla.addEventListener("click" , function(){
+
+    //scorro i due vettori con un indice che arriva fino ad una delle due lunghezze
+    //essendo identiche
+for(let i = 0 ; i < estrattiComputer.length ; i++){
+    totaleComputer += estrattiComputer[i];
+    totaleGiocatore += estrattiGiocatore[i];
+}
+
+if(totaleComputer > totaleGiocatore)
+    console.log("HA VINTO IL COMPUTER con i seguenti numeri" + totaleComputer);
+else
+    console.log("HA VINTO IL GIOCATORE CON I SEGUENTI NUMERI" + totaleGiocatore);
+
+});
